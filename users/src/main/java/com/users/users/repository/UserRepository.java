ee7d553002e02c72dce96f80227a6f360228c837
sergifercla <101187391+sergifercla@users.noreply.github.com>
2023-04-13 19:35:39 +0200

    aa

--- users/src/main/java/com/users/users/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.users.users.repository;
-
-import com.users.users.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
-
